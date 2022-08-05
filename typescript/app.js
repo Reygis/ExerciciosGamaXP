e1c66f3eecf93ef05e3f37a6414dc3a45162fdd5
Reygis Meira <reygis@outlook.com>
2022-08-04 21:28:10 -0300

    exercicios de typescript

---
+++ typescript/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let nome = 'Reygis';
+let idade = 33;
+let estado = 'Sergipe';
+let cidade = 'Aracaju';
+console.log(`Ola mundo! Meu nome é ${nome}, tenho ${idade} anos e sou de ${cidade} - ${estado}`);
---
+++ typescript/app.ts
@@ -0,0 +1,6 @@
+let nome: string = 'Reygis'
+let idade:number = 33
+let estado: string = 'Sergipe'
+let cidade: string = 'Aracaju'
+
+console.log(`Ola mundo! Meu nome é ${nome}, tenho ${idade} anos e sou de ${cidade} - ${estado}`)
